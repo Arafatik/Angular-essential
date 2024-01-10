135862a4642e8970b7f95e46d122f2b02311f441
Arafat Fayradov <arafatfayradov@gmail.com>
2024-01-10 19:15:26 +0500

    binding-html-to-ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,15 @@ export class AppComponent {
   arr = [1, 2, 3];
 
   obj = { a: 1, b: { c: 2 } };
+
+  img =
+    "https://styles.redditmedia.com/t5_2su6s/styles/communityIcon_4g1uo0kd87c61.png";
+
+  constructor() {
+    setTimeout(() => {
+      console.log("Timeout is over");
+      this.img =
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQc10uXFleZuTdeL4R0JYdMlHPpR0EiE1f6etcMoUr7tA&s";
+    }, 5000);
+  }
 }
