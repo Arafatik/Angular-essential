18f4326506880011e4b49f5018c07e2191030cc6
Arafat Fayradov <arafatfayradov@gmail.com>
2024-01-10 18:59:18 +0500

    interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'project';
+  title = "Dynamic title";
+  number = 42;
+  arr = [1, 2, 3];
+
+  obj = { a: 1, b: { c: 2 } };
 }
