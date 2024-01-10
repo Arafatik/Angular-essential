f434ceaa74fbbbdf9f6a0fa7b1681447beae1827
Arafat Fayradov <arafatfayradov@gmail.com>
2024-01-10 18:28:44 +0500

    html-and-scss-on-ts-file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { AppComponent } from "./app.component";
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { Post2Component } from "./post2/post2.component";
+import { Post3Component } from "./post3/post3.component";
+import { Post4Component } from "./post4/post4.component";
 import { PostComponent } from "./post/post.component";
-import { Post2Component } from './post2/post2.component';
-import { Post3Component } from './post3/post3.component';
 
 @NgModule({
-  declarations: [AppComponent, PostComponent, Post2Component, Post3Component],
+  declarations: [
+    AppComponent,
+    PostComponent,
+    Post2Component,
+    Post3Component,
+    Post4Component,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
