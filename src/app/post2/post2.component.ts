a6135b63a19d2f2542f1284ff6dc4e5e3a18acce
Arafat Fayradov <arafatfayradov@gmail.com>
2024-01-10 17:02:05 +0500

    add-new-components-with-cli

---
+++ src/app/post2/post2.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-post2",
+  templateUrl: "./post2.component.html",
+  styleUrls: ["./post2.component.scss"],
+})
+export class Post2Component {}
