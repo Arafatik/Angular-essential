3df11f1e1ece04957dddb99418ebc311d1b922c4
Arafat Fayradov <arafatfayradov@gmail.com>
2024-01-11 16:29:27 +0500

    add-ngstyle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,5 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = "Initial";
-
-  onInput(event: any) {
-    this.title = event.target.value;
-  }
+  backgroundToggle = false;
 }
