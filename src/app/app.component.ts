13e5516795bf744bd72e27f6f29fd252a2afc672
Arafat Fayradov <arafatfayradov@gmail.com>
2024-01-11 18:23:38 +0500

    work-with-ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  toggle: any = false;
+  arr = [1, 1, 2, 3, 5, 8, 13];
+
+  objects = [
+    {
+      title: "Post 1",
+      author: "Arafat",
+      comments: [
+        { name: "Petr", text: "Lorem 1" },
+        { name: "Sidr", text: "Lorem 2" },
+        { name: "Fedya", text: "Lorem 3" },
+      ],
+    },
+    {
+      title: "Post 2",
+      author: "Arafat 2",
+      comments: [
+        { name: "Petr 1", text: "Lorem 1" },
+        { name: "Sidr 1", text: "Lorem 2" },
+        { name: "Fedya 1", text: "Lorem 3" },
+      ],
+    },
+  ];
 }
