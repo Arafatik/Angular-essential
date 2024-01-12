7184a9124c88f4eae626ed554277decb2350b487
Arafat Fayradov <arafatfayradov@gmail.com>
2024-01-12 20:07:37 +0500

    access-for-html-elements

--- src/app/post-form/post-form.component.ts
+++ src/app/post-form/post-form.component.ts
@@ -1,4 +1,11 @@
-import { Component, EventEmitter, OnInit, Output } from "@angular/core";
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnInit,
+  Output,
+  ViewChild,
+} from "@angular/core";
 
 import { Post } from "../app.component";
 
@@ -9,6 +16,8 @@ import { Post } from "../app.component";
 })
 export class PostFormComponent implements OnInit {
   @Output() onAdd: EventEmitter<Post> = new EventEmitter<Post>();
+
+  @ViewChild("titleInput", { static: false }) inputRef: ElementRef;
 
   title = "";
   text = "";
@@ -26,5 +35,9 @@ export class PostFormComponent implements OnInit {
       this.onAdd.emit(post);
       this.title = this.text = "";
     }
+  }
+
+  focusTitle() {
+    this.inputRef.nativeElement.focus();
   }
 }
