ddb6c5a5079c68fb3674826f708a9b0e17013d81
Arafat Fayradov <arafatfayradov@gmail.com>
2024-01-16 17:02:08 +0500

    add-dynamics

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,10 +1,20 @@
-import { Directive, ElementRef, Renderer2 } from "@angular/core";
+import { Directive, ElementRef, HostListener, Renderer2 } from "@angular/core";
 
 @Directive({
   selector: "[appStyle]",
 })
 export class StyleDirective {
-  constructor(private el: ElementRef, private renderer: Renderer2) {
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  @HostListener("click", ["$event.target"]) onClick(event: Event) {
+    console.log(event);
+  }
+
+  @HostListener("mouseenter") onEnter() {
     this.renderer.setStyle(this.el.nativeElement, "color", "blue");
+  }
+
+  @HostListener("mouseleave") onLeave() {
+    this.renderer.setStyle(this.el.nativeElement, "color", null);
   }
 }
