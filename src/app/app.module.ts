101f0a71a020e48b3537be0e128336418c4dc9e2
Arafat Fayradov <arafatfayradov@gmail.com>
2024-01-16 20:33:26 +0500

    create-structure-directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,9 @@ import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 import { NgModule } from "@angular/core";
 import { StyleDirective } from "./directives/style.directive";
+import { IfnotDirective } from './directives/ifnot.directive';
 @NgModule({
-  declarations: [AppComponent, StyleDirective],
+  declarations: [AppComponent, StyleDirective, IfnotDirective],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
