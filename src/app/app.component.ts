855b9b33027762d2ae1f026666e3edbfe6e7cdca
Arafat Fayradov <arafatfayradov@gmail.com>
2024-01-20 14:06:24 +0500

    more-pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,17 @@ export class AppComponent {
   str = "hello world";
 
   date: Date = new Date();
+
+  float = 0.42;
+
+  obj = {
+    a: 1,
+    b: {
+      c: 2,
+      d: {
+        e: 3,
+        f: 4,
+      },
+    },
+  };
 }
