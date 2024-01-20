13b7fa9009a382e84f2936884dd6e7b7fbf10ad6
Arafat Fayradov <arafatfayradov@gmail.com>
2024-01-20 16:37:57 +0500

    list-filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,21 @@
 import { Component } from "@angular/core";
 
+export interface Post {
+  title: string;
+  text: string;
+}
+
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  e: number = Math.E;
-
-  str = "hello world";
-
-  date: Date = new Date();
-
-  float = 0.42;
-
-  obj = {
-    a: 1,
-    b: {
-      c: 2,
-      d: {
-        e: 3,
-        f: 4,
-      },
-    },
-  };
+  search = "";
+  searchField = "title";
+  posts: Post[] = [
+    { title: "Beer", text: "Самое лучшее пиво в мире" },
+    { title: "Bread", text: "The best bread in the world" },
+    { title: "Javascript", text: "The best language in the world" },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,9 @@ import { ExMarksPipe } from "./pipes/ex-marks.pipe";
 import { FormsModule } from "@angular/forms";
 import { MultByPipe } from "./pipes/mult-by.pipe";
 import { NgModule } from "@angular/core";
+import { FilterPipe } from './pipes/filter.pipe';
 @NgModule({
-  declarations: [AppComponent, MultByPipe, ExMarksPipe],
+  declarations: [AppComponent, MultByPipe, ExMarksPipe, FilterPipe],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
