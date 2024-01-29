6cbdc73937719f535bbb6d613d6cfdc5b6384075
Arafat Fayradov <arafatfayradov@gmail.com>
2024-01-29 20:17:55 +0500

    service-controller

--- src/app/services/app-counter.service.ts
+++ src/app/services/app-counter.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from "@angular/core";
+import { LogService } from "./log.service";
 
 @Injectable({
   providedIn: "root",
@@ -6,11 +7,15 @@ import { Injectable } from "@angular/core";
 export class AppCounterService {
   counter = 0;
 
+  constructor(private logService: LogService) {}
+
   increase() {
+    this.logService.log("increase counter...");
     this.counter++;
   }
 
   decrease() {
+    this.logService.log("decrease counter...");
     this.counter--;
   }
 }
---
+++ src/app/services/log.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({
+  providedIn: "root",
+})
+export class LogService {
+  log(text) {
+    console.log(`Log: ${text}`);
+  }
+}
