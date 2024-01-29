176e50aed4a7123f95204710af71c75ff996122a
Arafat Fayradov <arafatfayradov@gmail.com>
2024-01-29 18:41:53 +0500

    injectable-decorator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { AppComponent } from "./app.component";
-import { AppCounterService } from "./services/app-counter.service";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 import { NgModule } from "@angular/core";
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, FormsModule],
-  providers: [AppCounterService],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
