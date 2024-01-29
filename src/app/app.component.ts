9523c280783d427ba29834cd81129f41f8c0f47a
Arafat Fayradov <arafatfayradov@gmail.com>
2024-01-29 19:09:39 +0500

    locale-services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { AppCounterService } from "./services/app-counter.service";
 import { Component } from "@angular/core";
+import { LocalCounterService } from "./services/local-counter.service";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
+  providers: [LocalCounterService],
 })
 export class AppComponent {
-  constructor(private appCounterService: AppCounterService) {}
+  constructor(
+    private appCounterService: AppCounterService,
+    private localCounterService: LocalCounterService
+  ) {}
 }
