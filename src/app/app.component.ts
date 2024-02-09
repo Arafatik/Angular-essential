79a3636e4bb8bb5f49e9013b881123f75c9f5b68
Arafat Fayradov <arafatfayradov@gmail.com>
2024-02-09 19:09:54 +0500

    creating-form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,20 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+
+import { FormGroup } from "@angular/forms";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  form: FormGroup;
+
+  ngOnInit() {
+    this.form = new FormGroup({});
+  }
+
+  submit() {
+    console.log("Form submitted: ", this.form);
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+
 import { AppComponent } from "./app.component";
 import { BrowserModule } from "@angular/platform-browser";
-import { FormsModule } from "@angular/forms";
 import { NgModule } from "@angular/core";
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
